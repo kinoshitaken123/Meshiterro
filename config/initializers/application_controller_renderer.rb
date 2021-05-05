0ecd17bc2550beb7bb0ba13388794742c21fda7b
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-05-05 10:29:12 +0000

    [Update4-3]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ff2ef74d655cd826428d8e34077613f420bf92b935a39c8f1cf501a7c72d9b4c0cd08987b40a70a39f893c75cc65306f5f783ad56601eb95aa5c0c7968829cb2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_132814) do
+ActiveRecord::Schema.define(version: 2021_05_05_064600) do
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
     t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
