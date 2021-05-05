1d6df5ceb671e92bdc197e17e5287a5f9c4c7584
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-05-05 11:11:18 +0000

    [Update 4-4]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'post_images#index'
   devise_for :users
+  root 'post_images#index'
   resources :post_images,only: [:new,:create,:index,:show,:destroy]
+  resources :post_comments,only:[:create,:destroy]
 end
